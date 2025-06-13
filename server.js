7bd8c05cd9c00ea64de4ebb6d2b5de93a542dadf
primtive <aleksei792008@gmail.com>
2025-06-13 18:08:59 +0500

    init commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+
+// API endpoint для Next.js сервера
+app.post('/api/internal/record', (req, res) => {
+  const data = req.body;
+  broadcastToClients(data);
+  res.status(200).send({ success: true });
+});
+
+function broadcastToClients(message) {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
+
+// Запускаем сервер
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
